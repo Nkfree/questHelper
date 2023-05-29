245745bbcdfe57f431350a18dc5ad271f091e27a
Nkfree <BlackVaclav@gmail.com>
2023-05-29 22:12:11 +0200

    Update questItems.lua
    This database of quest items is a courtesy from Wujek.
    Previously this should have been exclusive to their server, however since a few years have already passed by, I dare to think it's safe to share it.

--- questItems.lua
+++ questItems.lua
@@ -1,7 +1,144 @@
 local questItems = {}
 
-
 -- MORROWIND QUEST ITEMS
-questItems["misc_dwrv_ark_cube00"] = "Dwemer puzzle box"
+questItems["bk_ajira1"] = {name = "Ajira's Mushroom Report", quest = "mg_stolenreport", questStage = 100, recordType = "book", special = true}
+questItems["bk_ajira2"] = {name = "Ajira's Flower Report", quest = "mg_stolenreport", questStage = 100, recordType = "book", special = true}
+questItems["lugrub's axe"] = {name = "Lugrub's Axe", quest = "il_widowland", questStage = 120, recordType = "weapon", special = false}
+questItems["bk_sottildescodebook"] = {name = "Sottilde's Code Book", quest = "fg_sottilde", questStage = 100, recordType = "book", special = true}
+questItems["dwarven war axe_redas"] = {name = "Redas War Axe", quest = "", questStage = 0, recordType = "weapon", special = false}
+questItems["ebony staff caper"] = {name = "Felen's Ebony Staff", quest = "", questStage = 0, recordType = "weapon", special = false}
+questItems["rusty_dagger_unique"] = {name = "A Rusty Dagger", quest = "", questStage = 0, recordType = "weapon", special = false}
+questItems["devil_tanto_tgamg"] = {name = "Anarenen's Devil Tanto", quest = "", questStage = 0, recordType = "weapon", special = false}
+questItems["bk_dispelrecipe_tgca"] = {name = "Dispel Potion Formula", quest = "tg_cookbookalchemy", questStage = 110, recordType = "book", special = false}
+questItems["bookskill_restoration1"] = {name = "Withershins", quest = "tg_withershins", questStage = 100, recordType = "book", special = false}
+questItems["daedric wakizashi_hhst"] = {name = "Daedric Wakizashi", quest = "", questStage = 0, recordType = "weapon", special = false}
+questItems["glass_dagger_enamor"] = {name = "Enamor", quest = "", questStage = 0, recordType = "weapon", special = false}
+questItems["fork_horripilation_unique"] = {name = "Fork of Horripilation", quest = "", questStage = 0, recordType = "weapon", special = false}
+questItems["dart_uniq_judgement"] = {name = "Dart of Judgment", quest = "", questStage = 0, recordType = "weapon", special = false}
+questItems["bonemold_gah-julan_hhda"] = {name = "Native Gah-Julan Bonemold Helm", quest = "", questStage = 0, recordType = "armor", special = false}
+questItems["bonemold_founders_helm"] = {name = "Redoran Founder's Helm", quest = "", questStage = 0, recordType = "armor", special = false}
+questItems["bonemold_tshield_hrlb"] = {name = "Redoran Banner Shield", quest = "", questStage = 0, recordType = "armor", special = false}
+questItems["amuletfleshmadewhole_uniq"] = {name = "Amulet of Flesh Made Whole", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["amulet_agustas_unique"] = {name = "Augustus' Amulet", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["expensive_amulet_delyna"] = {name = "Delyna's Locket", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["expensive_amulet_aeta"] = {name = "Expensive Amulet", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["sarandas_amulet"] = {name = "Firejade Amulet", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["exquisite_amulet_hlervu1"] = {name = "Hlervu Locket", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["julielle_aumines_amulet"] = {name = "Julielle Aumine's Amulet", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["linus_iulus_maran amulet"] = {name = "Linus Iulus' Maran Amulet", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["amulet_skink_unique"] = {name = "Skink's Amulet", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["linus_iulus_stendarran_belt"] = {name = "Linus Iulus' Stendarran Belt", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["sarandas_belt"] = {name = "Racer Suede Belt", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["common_glove_l_balmolagmer"] = {name = "Left Bal Molagmer Glove", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["common_glove_r_balmolagmer"] = {name = "Right Bal Molagmer Glove", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["extravagant_rt_art_wild"] = {name = "Gambolpuddy", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["expensive_glove_left_ilmeni"] = {name = "Ilmeni's glove", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["extravagant_glove_left_maur"] = {name = "Maurrie's Left Glove", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["extravagant_glove_right_maur"] = {name = "Maurrie's Right Glove", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["common_pants_02_hentus"] = {name = "Hentus Pants", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["sarandas_pants_2"] = {name = "Silk Pants", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["adusamsi's_ring"] = {name = "Adusamsi's Ring", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["extravagant_ring_aund_uni"] = {name = "Aundae Signet Ring", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["ring_blackjinx_uniq"] = {name = "Black Jinx", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["exquisite_ring_brallion"] = {name = "Brallion's Exquisite Ring", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["common_ring_danar"] = {name = "Danar's Ring", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["sarandas_ring_2"] = {name = "Ebony Ring", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["ring_keley"] = {name = "Engraved Ring of Healing", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["expensive_ring_01_bill"] = {name = "Expensive Ring", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["expensive_ring_aeta"] = {name = "Expensive Ring", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["sarandas_ring_1"] = {name = "Glass Ring", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["expensive_ring_01_hrdt"] = {name = "Nervion Ancestor Ring", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["exquisite_ring_processus"] = {name = "Processus Vitellius' Ring", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["extravagant_robe_01_red"] = {name = "Redas Robe of Deeds", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["robe of st roris"] = {name = "Robe of St Roris", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["exquisite_robe_drake's pride"] = {name = "Robe of the Drake's Pride", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["sarandas_shirt_2"] = {name = "Brocade Shirt", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["exquisite_shirt_01_rasha"] = {name = "Exquisite Shirt", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["sarandas_shoes_2"] = {name = "Designer Shoes", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["therana's skirt"] = {name = "Therana's Skirt", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["misc_beluelle_silver_bowl"] = {name = "Beluelle's Silver bowl", quest = "", questStage = 0, recordType = "miscellaneous", special = false}
+questItems["misc_lw_bowl_chapel"] = {name = "Chapel Limeware Bowl", quest = "", questStage = 0, recordType = "miscellaneous", special = false}
+questItems["misc_dwrv_artifact_ils"] = {name = "Drinar Varyon's Dwemer Tube", quest = "", questStage = 0, recordType = "miscellaneous", special = false}
+questItems["misc_dwarfbone_unique"] = {name = "Dwarven Bone", quest = "", questStage = 0, recordType = "miscellaneous", special = false}
+questItems["misc_dwrv_ark_cube00"] = {name = "Dwemer puzzle box", quest = "", questStage = 0, recordType = "miscellaneous", special = false}
+questItems["misc_fakesoulgem"] = {name = "Fake Soul Gem", quest = "", questStage = 0, recordType = "miscellaneous", special = false}
+questItems["ingred_guar_hide_girith"] = {name = "Girith's Guar Hide", quest = "", questStage = 0, recordType = "", special = false}
+questItems["misc_uniq_egg_of_gold"] = {name = "Golden Egg", quest = "", questStage = 0, recordType = "miscellaneous", special = false}
+questItems["misc_goblet_dagoth"] = {name = "House Dagoth cup", quest = "", questStage = 0, recordType = "miscellaneous", special = false}
+questItems["ingred_guar_hide_marsus"] = {name = "Marsus' Guar Hide", quest = "", questStage = 0, recordType = "", special = false}
+questItems["misc_6th_ash_hrmm"] = {name = "Morvayn Ash Statue", quest = "", questStage = 0, recordType = "miscellaneous", special = false}
+questItems["misc_de_goblet_01_redas"] = {name = "Redas Goblet", quest = "", questStage = 0, recordType = "miscellaneous", special = false}
+questItems["misc_skull_llevule"] = {name = "Skull of Llevule Andrano", quest = "", questStage = 0, recordType = "miscellaneous", special = false}
+questItems["misc_6th_ash_hrcs"] = {name = "Suspicious Ash Statue", quest = "", questStage = 0, recordType = "miscellaneous", special = false}
+questItems["misc_wraithguard_no_equip"] = {name = "Unique Dwemer Artifact", quest = "", questStage = 0, recordType = "miscellaneous", special = false}
+-- Sanguine Items
+questItems["sanguineamuletenterprise"] = {name = "Amulet of Sanguine Enterprise", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["sanguineamuletglibspeech"] = {name = "Amulet of Sanguine Glib Speech", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["sanguineamuletnimblearmor"] = {name = "Amulet of Sanguine Nimble Armor", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["sanguinebeltbalancedarmor"] = {name = "Belt of Sanguine Balanced Armor", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["sanguinebeltdeepbiting"] = {name = "Belt of Sanguine Deep Biting", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["sanguinebeltdenial"] = {name = "Belt of Sanguine Denial", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["sanguinebeltfleetness"] = {name = "Belt of Sanguine Fleetness", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["sanguinebelthewing"] = {name = "Belt of Sanguine Hewing", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["sanguinebeltimpaling"] = {name = "Belt of Sanguine Impaling Thrus", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["sanguinebeltmartialcraft"] = {name = "Belt of Sanguine Martial Craft", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["sanguinebeltsmiting"] = {name = "Belt of Sanguine Smiting", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["sanguinebeltstolidarmor"] = {name = "Belt of Sanguine Stolid Armor", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["sanguinebeltsureflight"] = {name = "Belt of Sanguine Sureflight", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["sanguinerglovehornyfist"] = {name = "Glove of Sanguine Horny Fist", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["sanguinelglovesafekeeping"] = {name = "Glove of Sanguine Safekeeping", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["sanguinergloveswiftblade"] = {name = "Glove of Sanguine Swiftblade", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["sanguineringfluidevasion"] = {name = "Ring of Sanguine Fluid Evasion", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["sanguineringgoldenw"] = {name = "Ring of Sanguine Golden Wisdom", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["sanguineringgreenw"] = {name = "Ring of Sanguine Green Wisdom", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["sanguineringredw"] = {name = "Ring of Sanguine Red Wisdom", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["sanguineringsilverw"] = {name = "Ring of Sanguine Silver Wisdom", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["sanguineringsublimew"] = {name = "Ring of Sanguine Sublime Wisdom", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["sanguineringtranscendw"] = {name = "Ring of Sanguine Transcendence", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["sanguineringtransfigurw"] = {name = "Ring of Sanguine Transfiguring", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["sanguineringunseenw"] = {name = "Ring of Sanguine Unseen Wisdom", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["sanguineshoesleaping"] = {name = "Shoes of Sanguine Leaping", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["sanguineshoesstalking"] = {name = "Shoes of Sanguine Stalking", quest = "", questStage = 0, recordType = "clothing", special = false}
+
+-- TRIBUNAL QUEST ITEMS
+
+questItems["ebony war axe_elanande"] = {name = "Elanande's War Axe", quest = "", questStage = 0, recordType = "weapon", special = false}
+questItems["dwarven mace_salandas"] = {name = "Suldreni's Mace", quest = "", questStage = 0, recordType = "weapon", special = false}
+questItems["silver dagger_droth_unique"] = {name = "Droth Dagger", quest = "", questStage = 0, recordType = "weapon", special = false}
+questItems["silver dagger_droth_unique_a"] = {name = "Droth Dagger", quest = "", questStage = 0, recordType = "weapon", special = false}
+questItems["ebony shortsword_soscean"] = {name = "Soscean's Shortsword", quest = "", questStage = 0, recordType = "weapon", special = false}
+questItems["silver spear_uvenim"] = {name = "Silver Spear", quest = "", questStage = 0, recordType = "weapon", special = false}
+questItems["ebony_cuirass_soscean"] = {name = "Soscean's Cuirass", quest = "", questStage = 0, recordType = "armor", special = false}
+questItems["silver_helm_uvenim"] = {name = "Imperial Silver Helm", quest = "", questStage = 0, recordType = "armor", special = false}
+questItems["amulet_salandas"] = {name = "Suldreni's Amulet", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["extravagant_robe_02_elanande"] = {name = "Elanande's Robe", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["bladepiece_02"] = {name = "Broken Dwemer Blade Piece", quest = "", questStage = 0, recordType = "miscellaneous", special = false}
+questItems["bladepiece_03"] = {name = "Broken Dwemer Blade Piece", quest = "", questStage = 0, recordType = "miscellaneous", special = false}
+questItems["dwemer_satchel00"] = {name = "Dwemer Satchel Pack", quest = "", questStage = 0, recordType = "miscellaneous", special = false}
+questItems["misc_dwrv_weather"] = {name = "Powered Dwemer Coherer", quest = "", questStage = 0, recordType = "miscellaneous", special = false}
+questItems["misc_dwrv_weather2"] = {name = "Powered Dwemer Coherer", quest = "", questStage = 0, recordType = "miscellaneous", special = false}
+questItems["bk_playscript"] = {name = "The Horror of Castle Xyr", quest = "", questStage = 0, recordType = "book", special = true}
+questItems["sc_chridittepanacea"] = {name = "Scroll of Chriditte's Panacea", quest = "", questStage = 0, recordType = "book", special = true}
+
+
+-- BLOODMOON QUEST ITEMS
+
+questItems["bm nordic pick"] = {name = "Ancient Nordic Pick Axe", quest = "", questStage = 0, recordType = "weapon", special = false}
+questItems["steel arrow_carnius"] = {name = "Poisoned Arrow", quest = "", questStage = 0, recordType = "weapon", special = false}
+questItems["steel longbow_carnius"] = {name = "Steel Longbowsteel ", quest = "", questStage = 0, recordType = "weapon", special = false}
+questItems["saber_elberoth"] = {name = "Elberoth's Saber", quest = "", questStage = 0, recordType = "weapon", special = false}
+questItems["imperial shortsword severio"] = {name = "Severia's Imperial Shortsword", quest = "", questStage = 0, recordType = "weapon", special = false}
+questItems["bm silver dagger wolfender"] = {name = "Wolfender Silver Dagger", quest = "", questStage = 0, recordType = "weapon", special = false}
+questItems["bm_dagger_wolfgiver"] = {name = "Wolfgiver Silver Dagger", quest = "", questStage = 0, recordType = "weapon", special = false}
+questItems["fur_colovian_helm_white"] = {name = "White Colovian Fur Helm", quest = "", questStage = 0, recordType = "armor", special = false}
+questItems["amulet of infectious charm"] = {name = "Amulet of Infectious Charm", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["expensive_ring_erna"] = {name = "Erna the Quiet's Ring", quest = "", questStage = 0, recordType = "clothing", special = false}
+questItems["bm_bearheart_unique"] = {name = "Heart of the Spirit Bear", quest = "", questStage = 0, recordType = "miscellaneous", special = false}
+questItems["misc_skull_oddfrid"] = {name = "Oddfrid White-Lip", quest = "", questStage = 0, recordType = "miscellaneous", special = false}
+questItems["misc_skull_skaal"] = {name = "Skull of a Skaal Warrior", quest = "", questStage = 0, recordType = "miscellaneous", special = false}
+questItems["bm_seeds_unique"] = {name = "Strange Seeds", quest = "", questStage = 0, recordType = "miscellaneous", special = false}
+questItems["misc_bm_clawfang_unique"] = {name = "Totem of Claw and Fang", quest = "", questStage = 0, recordType = "miscellaneous", special = false}
+questItems["bm_waterlife_unique1"] = {name = "Waters of Life", quest = "", questStage = 0, recordType = "miscellaneous", special = false}
+
 
 return questItems
